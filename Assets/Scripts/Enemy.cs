afca3da39645cf6a39c7a48d05d45af944ee885e
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-07-05 10:38:59 -0400

    enemy collision destroy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,27 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     
-    }
 
     private void FixedUpdate()
     {
         transform.Rotate(0,0,rotationSpeed);
     }
 
+    public void OnTriggerEnter2d(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    
+        else if(collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    
+    }
 
 
 }
