3a10d85c5eae2303d459fbd30d12c5ef93e15d71
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-07-08 10:14:21 -0400

    Added dust particles on collision to enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float rotationSpeed;
-    
+    //dust
+    public GameObject dust;
 
     private void FixedUpdate()
     {
@@ -20,8 +21,15 @@ public class Enemy : MonoBehaviour
         }
     
         else if(collision.gameObject.tag == "Ground")
-        {
-            Destroy(gameObject);
+        {   
+            gameObject.SetActive(false);
+            //dust at position of spike collision
+            GameObject dustEffect = Instantiate(dust,transform.position,Quaternion.identity);
+            //destroys dust effect after short time
+            
+            Destroy(dustEffect, 2f);
+            
+            Destroy(gameObject, 3f);
         }
     
     }
