7d96a25b90977ad6c3cdf734f0f314a5a10f4b3e
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-07-12 17:04:15 -0400

    fixed but in enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,22 +13,23 @@ public class Enemy : MonoBehaviour
         transform.Rotate(0,0,rotationSpeed);
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Destroy(collision.gameObject);
+            Gamemanager.instance.GameOver();
         }
     
         else if(collision.gameObject.tag == "Ground")
-        {   
+        {   //adds to score
+            Gamemanager.instance.IncrementScore();            
             gameObject.SetActive(false);
             //dust at position of spike collision
             GameObject dustEffect = Instantiate(dust,transform.position,Quaternion.identity);
             //destroys dust effect after short time
             
             Destroy(dustEffect, 2f);
-            
             Destroy(gameObject, 3f);
         }
     
--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class Gamemanager : MonoBehaviour
 {
     // static instance of game manager
 
-    public static GameManager instance;
+    public static Gamemanager instance;
     bool gameOver = false;
+    int score = 0;
+    public Text scoreText;
     private void Awake()
     {
         if(instance == null)
@@ -21,6 +24,14 @@ public class GameManager : MonoBehaviour
         gameOver = true;
         GameObject.Find("EnemySpawn").GetComponent<EnemySpawner>().StopSpawning();
     }
-
+    public void IncrementScore()
+    {
+        if (!gameOver)
+        {
+            score++;
+            print(score);
+        }
+        scoreText.text = score.ToString();
+    }
 
 }
